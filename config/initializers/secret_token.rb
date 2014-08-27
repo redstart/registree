c9e8129922c653a307b486ac8b8488d225fe0871
redstartme@gmail.com <redstartme@gmail.com>
2014-08-27 16:36:32 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Registree::Application.config.secret_token = 'a8421fd50395b299ca8c6482838693dcbd91d0a53563be0ba9c1ace687f1a32f05ec3fbf035b76a66216a07d2135e6aad8903a35592a82eb09f28b18ecd5014c'
